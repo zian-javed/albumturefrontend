8387bb90392ba8f0f5a6107be9bff2bf8ca57cfe
APerezBanks <88187553+APerezBanks@users.noreply.github.com>
2022-03-29 10:45:06 +0100

    Tuesday morning update

--- src/components/userPage/userPage.js
+++ src/components/userPage/userPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import NavBar from '../navbar/navBar.js';
 import { Link } from "react-router-dom";
-import Spotify from '../spotifyAPI.js';
+
 import './userPage.css';
 
 const UserContent = ({user}) => {
@@ -11,7 +11,7 @@ const UserContent = ({user}) => {
         <div className='userContent'>
             <NavBar user={user} />
             <h1> this is the user page </h1>
-            <Spotify/>
+         
             <Link to="/addToListPage">Let's add more</Link>
         </div>
          <div>
