7d3ea829e8a1df4be99a845587312320a3cd7e1a
APerezBanks <88187553+APerezBanks@users.noreply.github.com>
2022-03-30 15:16:37 +0100

    work please

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { createUser, login } from "../../utils"; 
 import { Navigate } from "react-router-dom";
 import './login.css';
+import UserContent from "../userPage/userPage";
 
 export const Login = ({user, setUser}) => {
   const [username, setUsername] = useState();
@@ -28,62 +29,58 @@ export const Login = ({user, setUser}) => {
   }
 };
   return (
-    
-    <div className="loginpage">
-     
+    <div> <UserContent/> </div>
+    // <div className="loginpage">
+    //    {/* <NavBar user={user} /> */}
 
-    <div className="welcome">
-          <img src={bool ? "./images/photo4.png" : "./images/photo1.png"} alt="left menu" ></img>      
-    </div>
+    // <div className="welcome">
+    //       <img src={bool ? "./images/photo4.png" : "./images/photo1.png"} alt="left menu" ></img>      
+    // </div>
 
-    <div>
+    // <div>
 
-      <div className="logText"><h1> {name} </h1> </div>
+    //   <div className="logText"><h1> {name} </h1> </div>
 
-      {user && <Navigate to="/collectionPage" />}
+    //   {user && <Navigate to="/collectionPage" />}
 
-      <form className="formlog" onSubmit={submitHandler}>
+    //   <form className="formlog" onSubmit={submitHandler}>
+    //       <input onChange={(event) => setUsername(event.target.value)}
+    //       placeholder = "  username " />
+    //     <br></br> 
+
+    //     {/* <label>username</label>  */}
+
+    //     <br></br>
         
-        <input onChange={(event) => setUsername(event.target.value)}
-          placeholder = "  username " />
-        <br></br> 
-        <br></br>
-        <br></br>
-        
-        
-        {!bool && 
-          <>
-          <input onChange={(event) => setEmail(event.target.value) } type="email"
-          placeholder = "  email "/>
-          <br></br>
-          <br></br>
-          <br></br>
-        
-          </>
-          }
+    //     {!bool && 
+    //       <>
+    //       <input onChange={(event) => setEmail(event.target.value) } type="email"
+    //       placeholder = "  email "/>
+    //       <br></br>
+    //       {/* <label>email</label> */}
+    //       <br></br>
+    //       </>
+    //       }
           
-          <input onChange={(event) => setPass(event.target.value) } type="password"
-          placeholder = "  password " />
-          <br></br>
-          {/* <label>password</label>  */}
-          <br></br>
-          <br></br>
-          <br></br>
-          <button className="logB" type="submit">{!bool ? " Press to create account " : "Login"} </button>
-          <br></br>
-          <br></br>
-          <br></br>
-        </form>
+    //       <input onChange={(event) => setPass(event.target.value) } type="password"
+    //       placeholder = "  password " />
+    //       <br></br>
+    //       {/* <label>password</label>  */}
+    //       <br></br>
+    //       <button type="submit">{!bool ? " Press to create account " : "Login"} </button>
+    //       <br></br>
+    //       <br></br>
+    //     </form>
 
        
 
-        <label className="label">{!bool ? " Already have an account? " : " Don't have an account?  " } </label>
+    //     <label className="label">{!bool ? " Already have an account? " : " Don't have an account?  " } </label>
 
-        <button className="logV" onClick={handleClick}> Click here! </button>
+    //     <button onClick={handleClick}> Click here! </button>
 
 
-      </div>
-    </div>
+    //   </div>
+    // </div>
   )
 };
 
--- src/components/userPage/userPage.js
+++ src/components/userPage/userPage.js
@@ -7,31 +7,37 @@ import './userPage.css';
 const UserContent = ({user, setUser}) => {
    
     return (
-        <>
-        <div className='userContent'>
+        <div>
             <NavBar user={user} setUser={setUser} />
-            <h1> this is the user page </h1>
-         
-            <Link to="/addToListPage">Let's add more</Link>
-        </div>
-         <div>
-             <h2>Suggested Albums</h2>
-         </div>
-        <div className="containerAlbum">
+            <div className='userContent'>
+                <div className='leftCol'>
+                <p> Welcome, friend! {user} </p>
+                </div>
+                <div className='rightCol'>
+                <Link id='link1' to="/addToListPage">Search for Albums</Link>
+                </div>
+                
             
-              <img src='./images/Adele.jpeg' alt="Adele" />
-              <img src='./images/deathM.png' alt="Death Metal" />
-              <img src='./images/Kate.jpg' alt="Kate" />
-             
-              <img src='./images/Weird.jpeg' alt="Weird" />
-              <img src='./images/Skrillex.jpeg' alt="Skrillex" />
-              <img src='./images/Smurfs.jpeg' alt="Smurfs" />
-             
+                
+            </div>
+            
+
+            <div className='suggestion'>
+                <h3>Suggested Albums</h3>
+                <div className="containerAlbum">
+                    <img src='./images/Adele.jpeg' alt="Adele" />
+                    <img src='./images/deathM.png' alt="Death Metal" />
+                    <img src='./images/Kate.jpg' alt="Kate" />
+                    <img src='./images/Weird.jpeg' alt="Weird" />
+                    <img src='./images/Skrillex.jpeg' alt="Skrillex" />
+                    <img src='./images/Smurfs.jpeg' alt="Smurfs" />
+                </div>
+            </div>
+
+
 
 
         </div>
-
-        </>
     );
 };
 
