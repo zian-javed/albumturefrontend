0887fbd7ec24b8ef6038ed67280c3391214cc12f
Zian Waqas Javed <zianjaved@hotmail.com>
2022-03-30 15:12:44 +0100

    hotfix

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { createUser, login } from "../../utils"; 
 import { Navigate } from "react-router-dom";
 import './login.css';
-import UserContent from "../userPage/userPage";
 
 export const Login = ({user, setUser}) => {
   const [username, setUsername] = useState();
@@ -29,58 +28,58 @@ export const Login = ({user, setUser}) => {
   }
 };
   return (
-    <div> <UserContent/> </div>
-    // <div className="loginpage">
-    //    {/* <NavBar user={user} /> */}
+    
+    <div className="loginpage">
+       {/* <NavBar user={user} /> */}
 
-    // <div className="welcome">
-    //       <img src={bool ? "./images/photo4.png" : "./images/photo1.png"} alt="left menu" ></img>      
-    // </div>
+    <div className="welcome">
+          <img src={bool ? "./images/photo4.png" : "./images/photo1.png"} alt="left menu" ></img>      
+    </div>
 
-    // <div>
+    <div>
 
-    //   <div className="logText"><h1> {name} </h1> </div>
+      <div className="logText"><h1> {name} </h1> </div>
 
-    //   {user && <Navigate to="/collectionPage" />}
+      {user && <Navigate to="/collectionPage" />}
 
-    //   <form className="formlog" onSubmit={submitHandler}>
-    //       <input onChange={(event) => setUsername(event.target.value)}
-    //       placeholder = "  username " />
-    //     <br></br> 
+      <form className="formlog" onSubmit={submitHandler}>
+          <input onChange={(event) => setUsername(event.target.value)}
+          placeholder = "  username " />
+        <br></br> 
 
-    //     {/* <label>username</label>  */}
+        {/* <label>username</label>  */}
 
-    //     <br></br>
+        <br></br>
         
-    //     {!bool && 
-    //       <>
-    //       <input onChange={(event) => setEmail(event.target.value) } type="email"
-    //       placeholder = "  email "/>
-    //       <br></br>
-    //       {/* <label>email</label> */}
-    //       <br></br>
-    //       </>
-    //       }
+        {!bool && 
+          <>
+          <input onChange={(event) => setEmail(event.target.value) } type="email"
+          placeholder = "  email "/>
+          <br></br>
+          {/* <label>email</label> */}
+          <br></br>
+          </>
+          }
           
-    //       <input onChange={(event) => setPass(event.target.value) } type="password"
-    //       placeholder = "  password " />
-    //       <br></br>
-    //       {/* <label>password</label>  */}
-    //       <br></br>
-    //       <button type="submit">{!bool ? " Press to create account " : "Login"} </button>
-    //       <br></br>
-    //       <br></br>
-    //     </form>
+          <input onChange={(event) => setPass(event.target.value) } type="password"
+          placeholder = "  password " />
+          <br></br>
+          {/* <label>password</label>  */}
+          <br></br>
+          <button type="submit">{!bool ? " Press to create account " : "Login"} </button>
+          <br></br>
+          <br></br>
+        </form>
 
        
 
-    //     <label className="label">{!bool ? " Already have an account? " : " Don't have an account?  " } </label>
+        <label className="label">{!bool ? " Already have an account? " : " Don't have an account?  " } </label>
 
-    //     <button onClick={handleClick}> Click here! </button>
+        <button onClick={handleClick}> Click here! </button>
 
 
-    //   </div>
-    // </div>
+      </div>
+    </div>
   )
 };
 
