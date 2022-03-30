a178aa993c05725694e666a09567b93ae26d0b55
APerezBanks <88187553+APerezBanks@users.noreply.github.com>
2022-03-31 00:23:59 +0100

    user page and nav bar redesigned

--- src/components/userPage/userPage.js
+++ src/components/userPage/userPage.js
@@ -8,36 +8,60 @@ const UserContent = ({user, setUser}) => {
    
     return (
         <div>
+             
+            <div>
             <NavBar user={user} setUser={setUser} />
+            </div>
+           
             <div className='userContent'>
+
                 <div className='leftCol'>
-                <p> Welcome, friend! {user} </p>
+                <img src='./images/10a.png' alt="Adele" />
+                {/* <p> Welcome, friend! {user} </p> */}
                 </div>
                 <div className='rightCol'>
+                <div className="container">
+                <p className="title">
+                    <span className="title-word title-word-1">Welcome </span>
+                    <span className="title-word title-word-2"> to</span>
+                    <span className="title-word title-word-3"> Albumture</span>
+                    <br></br>
+                    <span className="title-word title-word-4">{user}</span>
+                </p>
+                {/* <p className='texta'> Welcome, friend! {user} </p> */}
+                <p className="textb">Click bellow to search for new music</p>
                 <Link id='link1' to="/addToListPage">Search for Albums</Link>
+             
+                {/* <img src='./images/6a.png' alt="cool" /> */}
+
                 </div>
                 
-            
-                
             </div>
-            
+            <div>
+               
+                </div>
+            </div>
+          
 
-            <div className='suggestion'>
-                <h3>Suggested Albums</h3>
-                <div className="containerAlbum">
+                    
+                    <div className='suggestion'>
+                
+                    <div className="containerAlbum">
                     <img src='./images/Adele.jpeg' alt="Adele" />
                     <img src='./images/deathM.png' alt="Death Metal" />
                     <img src='./images/Kate.jpg' alt="Kate" />
                     <img src='./images/Weird.jpeg' alt="Weird" />
                     <img src='./images/Skrillex.jpeg' alt="Skrillex" />
                     <img src='./images/Smurfs.jpeg' alt="Smurfs" />
+                    </div>
+                     {/* <div className="sugtext"><p>Suggested Albums</p></div> */}
                 </div>
-            </div>
+    </div>
 
 
 
 
-        </div>
+       
     );
 };
 
